85f0be00f9870a967a481fc22246ff7eca3febf8
Alex Tanas <atanas@student.ubc.ca>
2026-02-07 17:59:48 -0800

    Started importing plant data (thank you West Coast Seeds)

---
+++ plantPlantTime.py
@@ -0,0 +1,266 @@
+planting_data = {
+    # --- Vegetables (from Images 4 & 5) ---
+    "Amaranth": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Asparagus": {
+        "start": [2.5, 5.5],      # Mid-Feb to Mid-May
+        "transplant": [5.5, 6.5], # Mid-May to Mid-Jun
+        "direct_sow": []
+    },
+    "Beans": { # Mapped to 'Bean, bush & pole'
+        "start": [],
+        "transplant": [],
+        "direct_sow": [5.5, 7.5]  # Mid-May to Mid-Jul
+    },
+    "Broad Beans": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [2.0, 4.5, 9.0, 11.0] # Feb 1-Mid-Apr, Sep 1-Nov 1
+    },
+    "Soya Beans": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Beet": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [4.0, 8.5]  # Apr 1 to Mid-Aug
+    },
+    "Broccoli": {
+        "start": [3.0, 4.5],      # Mar 1 to Mid-Apr
+        "transplant": [4.5, 6.0], # Mid-Apr to Jun 1
+        "direct_sow": []
+    },
+    "Brussels Sprouts": {
+        "start": [5.5, 6.5],      # Mid-May to Mid-Jun
+        "transplant": [6.5, 7.5], # Mid-Jun to Mid-Jul
+        "direct_sow": []
+    },
+    "Cabbage": {
+        "start": [3.0, 4.5],      # Mar 1 to Mid-Apr
+        "transplant": [4.5, 6.0], # Mid-Apr to Jun 1
+        "direct_sow": []
+    },
+    "Carrots": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [4.0, 7.5]  # Apr 1 to Mid-Jul
+    },
+    "Cauliflower": {
+        "start": [3.0, 4.5],      # Mar 1 to Mid-Apr
+        "transplant": [4.5, 6.5], # Mid-Apr to Mid-Jun
+        "direct_sow": []
+    },
+    "Celery": {
+        "start": [3.0, 4.5],      # Mar 1 to Mid-Apr
+        "transplant": [5.5, 6.5], # Mid-May to Mid-Jun
+        "direct_sow": []
+    },
+    "Collards": { # Mapped to 'Kale & Collard'
+        "start": [],
+        "transplant": [],
+        "direct_sow": [2.0, 8.5]  # Feb 1 to Mid-Aug
+    },
+    "Corn": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Cucumber": {
+        "start": [4.5, 5.5],      # Mid-Apr to Mid-May
+        "transplant": [5.5, 6.5], # Mid-May to Mid-Jun
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Eggplant": {
+        "start": [3.5, 4.5],      # Mid-Mar to Mid-Apr
+        "transplant": [5.5, 6.5], # Mid-May to Mid-Jun
+        "direct_sow": []
+    },
+    "Fennel": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [3.5, 6.5]  # Mid-Mar to Mid-Jun
+    },
+    "Garlic": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [2.0, 2.25, 9.0, 13.0] # Feb 1-Feb ~8, Sep 1-Dec 31
+    },
+    "Kale": { # Mapped to 'Kale & Collard'
+        "start": [],
+        "transplant": [],
+        "direct_sow": [2.0, 8.5]  # Feb 1 to Mid-Aug
+    },
+    "Kohlrabi": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [3.5, 5.5, 7.5, 8.25] # Mid-Mar to Mid-May, Mid-Jul to Early Aug
+    },
+    "Leeks": {
+        "start": [1.5, 4.0, 6.0, 6.5], # Mid-Jan to Apr 1, Jun 1 to Mid-Jun
+        "transplant": [4.0, 5.5, 7.0, 7.5], # Apr 1 to Mid-May, Jul 1 to Mid-Jul
+        "direct_sow": []
+    },
+    "Lettuce": {
+        "start": [2.0, 4.0],      # Feb 1 to Apr 1
+        "transplant": [3.5, 4.5], # Mid-Mar to Mid-Apr
+        "direct_sow": [4.5, 9.5]  # Mid-Apr to Mid-Sep
+    },
+    "Melons": {
+        "start": [4.5, 5.5],      # Mid-Apr to Mid-May
+        "transplant": [5.5, 6.5], # Mid-May to Mid-Jun
+        "direct_sow": []
+    },
+    "Mustard": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [2.0, 6.0, 9.0, 10.5] # Feb 1 to Jun 1, Sep 1 to Mid-Oct
+    },
+    "Onions": {
+        "start": [1.0, 4.5],      # Jan 1 to Mid-Apr
+        "transplant": [],
+        "direct_sow": [3.5, 5.5]  # Mid-Mar to Mid-May
+    },
+    "Peas": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [2.0, 5.5, 6.5, 8.5] # Feb 1 to Mid-May, Mid-Jun to Mid-Aug
+    },
+    "Pepper": {
+        "start": [3.0, 4.5],      # Mar 1 to Mid-Apr
+        "transplant": [5.5, 6.5], # Mid-May to Mid-Jun
+        "direct_sow": []
+    },
+    "Radish": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [2.0, 5.5, 9.0, 10.0] # Feb 1 to Mid-May, Sep 1 to Oct 1
+    },
+    "Spinach": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [2.0, 5.5, 9.0, 10.0] # Feb 1 to Mid-May, Sep 1 to Oct 1
+    },
+    "Squash": {
+        "start": [4.5, 5.5],      # Mid-Apr to Mid-May
+        "transplant": [5.5, 6.5], # Mid-May to Mid-Jun
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Strawberries": {
+        "start": [1.0, 3.5],      # Jan 1 to Mid-Mar
+        "transplant": [4.5, 5.5], # Mid-Apr to Mid-May
+        "direct_sow": []
+    },
+    "Swiss Chard": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [4.5, 7.5]  # Mid-Apr to Mid-Jul
+    },
+    "Tomatoes": {
+        "start": [3.0, 4.5],      # Mar 1 to Mid-Apr
+        "transplant": [4.5, 6.5], # Mid-Apr to Mid-Jun
+        "direct_sow": []
+    },
+    "Turnip": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [3.5, 5.5, 8.5, 10.0] # Mid-Mar to Mid-May, Mid-Aug to Oct 1
+    },
+
+    # --- Herbs (from Image 6) ---
+    "Basil": {
+        "start": [4.0, 5.5],      # Apr 1 to Mid-May
+        "transplant": [5.5, 6.5], # Mid-May to Mid-Jun
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Borage": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [4.0, 8.0]  # Apr 1 to Aug 1
+    },
+    "Chamomile": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [4.0, 6.0, 9.0, 11.0] # Apr 1 to Jun 1, Sep 1 to Nov 1
+    },
+    "Chervil": {
+        "start": [3.5, 4.0],      # Mid-Mar to Apr 1
+        "transplant": [],
+        "direct_sow": [4.0, 8.0]  # Apr 1 to Aug 1
+    },
+    "Chives": {
+        "start": [2.0, 4.5],      # Feb 1 to Mid-Apr
+        "transplant": [4.5, 5.5], # Mid-Apr to Mid-May
+        "direct_sow": [4.5, 6.5, 8.5, 9.5] # Mid-Apr to Mid-Jun, Mid-Aug to Mid-Sep
+    },
+    "Cilantro": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [3.0, 9.5]  # Mar 1 to Mid-Sep
+    },
+    "Dill": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [4.5, 8.5]  # Mid-Apr to Mid-Aug
+    },
+    "Lovage": {
+        "start": [3.5, 4.0],      # Mid-Mar to Apr 1
+        "transplant": [5.5, 6.0], # Mid-May to Jun 1
+        "direct_sow": [4.0, 5.5, 9.0, 11.0] # Apr 1-Mid-May, Sep 1-Nov 1
+    },
+    "Mint": {
+        "start": [2.0, 4.5],      # Feb 1 to Mid-Apr
+        "transplant": [4.5, 6.5], # Mid-Apr to Mid-Jun
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Oregano": {
+        "start": [2.5, 5.5],      # Mid-Feb to Mid-May
+        "transplant": [4.5, 6.5], # Mid-Apr to Mid-Jun
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Sweet Marjoram": { # Mapped to 'Oregano & Marjoram'
+        "start": [2.5, 5.5],      # Mid-Feb to Mid-May
+        "transplant": [4.5, 6.5], # Mid-Apr to Mid-Jun
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Parsley": {
+        "start": [],
+        "transplant": [],
+        "direct_sow": [3.5, 8.0]  # Mid-Mar to Aug 1
+    },
+    "Rosemary": {
+        "start": [2.0, 5.5],      # Feb 1 to Mid-May
+        "transplant": [4.5, 6.5], # Mid-Apr to Mid-Jun
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Sage": {
+        "start": [2.0, 5.5],      # Feb 1 to Mid-May
+        "transplant": [4.5, 6.5], # Mid-Apr to Mid-Jun
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Summer Savory": {
+        "start": [3.5, 5.5],      # Mid-Mar to Mid-May
+        "transplant": [5.5, 6.0], # Mid-May to Jun 1
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+    "Thyme": {
+        "start": [2.0, 5.5],      # Feb 1 to Mid-May
+        "transplant": [4.5, 6.5], # Mid-Apr to Mid-Jun
+        "direct_sow": [5.5, 6.5]  # Mid-May to Mid-Jun
+    },
+
+    # --- Missing from Schedule Charts ---
+    "Agastache": { "start": [], "transplant": [], "direct_sow": [] },
+    "Calendula": { "start": [], "transplant": [], "direct_sow": [] },
+    "Catnip": { "start": [], "transplant": [], "direct_sow": [] },
+    "Clover": { "start": [], "transplant": [], "direct_sow": [] },
+    "Nasturtiums": { "start": [], "transplant": [], "direct_sow": [] },
+    "Oats": { "start": [], "transplant": [], "direct_sow": [] },
+    "Phacelia": { "start": [], "transplant": [], "direct_sow": [] },
+    "Potatoes": { "start": [], "transplant": [], "direct_sow": [] },
+    "Sunflowers": { "start": [], "transplant": [], "direct_sow": [] }
+}
